ca5ac2c49de7d80638a5fdecb49a9514117e6267
jonys124 <jonys.m@seznam.cz>
2022-11-22 18:43:30 +0100

    created expression.c/h

--- generator.h
+++ generator.h
@@ -5,13 +5,6 @@
 #ifndef IFJCODE22_GENERATOR_H
 #define IFJCODE22_GENERATOR_H
 #include <stdbool.h>
-#include "token.h"
-#include "stack.h"
 
-
-int priority(tToken *token, tStack * stack);
-int token_type(tToken * token);
-void infix_to_postfix(tlist * list);
-void tri_code_gen(tlist * list);
 
 #endif //IFJCODE22_GENERATOR_H
