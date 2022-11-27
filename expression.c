a31c6540ce60f7f22b919fd86f1705f00159578c
Ondřej Koumar <ondrejkoumar@seznam.cz>
2022-11-27 15:50:40 +0100

    Yet another progress in expression.
    Finally started working on stack reductions.
    Co-authored-by: jonys124 <jonys124@users.noreply.github.com>
    Co-authored-by: MeRohel <MeRohel@users.noreply.github.com>

--- expression.c
+++ expression.c
@@ -29,7 +29,7 @@ const char prd_table[15][15] = {
     {'<','<','<','<','<','>','>','>','>','>','>','<','>','<','>'},  // >=
     {'<','<','<','<','<','<','<','<','<','>','>','<','>','<','>'},  // ===
     {'<','<','<','<','<','<','<','<','<','>','>','<','>','<','>'},  // !==
-    {'<','<','<','<','<','<','<','<','<','<','<','<','x','<','x'},  // (
+    {'<','<','<','<','<','<','<','<','<','<','<','<','=','<','x'},  // (
     {'>','>','>','>','>','>','>','>','>','>','>','x','>','x','>'},  // )
     {'>','>','>','>','>','>','>','>','>','>','>','x','>','x','>'},  // id
     {'<','<','<','<','<','<','<','<','<','<','<','<','x','<','x'}   // $
@@ -37,44 +37,33 @@ const char prd_table[15][15] = {
 
 bool isOperator(tToken *token)
 {
-    if (token->type == tMul || token->type == tDiv || token->type == tPlus || token->type == tMinus || token->type == tLess || token->type == tMore ||
-        token->type == tLessEq || token->type == tMoreEq || token->type == tIdentical || token->type == tNotIdentical || token->type == tConcat)
-        return true;
-    else
-        return false;
+    return (token->type == tMul || token->type == tDiv || token->type == tPlus || token->type == tMinus || token->type == tLess || token->type == tMore ||
+        token->type == tLessEq || token->type == tMoreEq || token->type == tIdentical || token->type == tNotIdentical || token->type == tConcat);
 }
 
 bool numberOp(tToken *token)
 {
-    if (token->type == tMul || token->type == tDiv || token->type == tPlus || token->type == tMinus || token->type == tLess || token->type == tMore ||
-        token->type == tLessEq || token->type == tMoreEq || token->type == tIdentical || token->type == tNotIdentical)
-        return true;
-    else
-        return false;
+    return (token->type == tMul || token->type == tDiv || token->type == tPlus || token->type == tMinus);
 }
 
 bool relationalOp(tToken *token)
 {
-    if (token->type == tLess || token->type == tMore || token->type == tLessEq || token->type == tMoreEq || token->type == tIdentical || token->type == tNotIdentical)
-        return true;
-    else
-        return false;
+    return (token->type == tLess || token->type == tMore || token->type == tLessEq || token->type == tMoreEq || token->type == tIdentical || token->type == tNotIdentical);
 }
 
 bool stringOp(tToken *token)
 {
-    if (token->type == tIdentical || token->type == tNotIdentical || token->type == tConcat)
-        return true;
-    else
-        return false;
+    return (token->type == tIdentical || token->type == tNotIdentical || token->type == tConcat);
 }
 
-bool isVariableOrConst(tToken *token)
+bool isConst(tToken *token)
 {
-    if (token->type == tIdentifier || token->type == tInt || token->type == tInt2 || token->type == tReal || token->type == tReal2 || token->type == tLiteral)
-        return true;
-    else
-        return false;
+    return (token->type == tInt || token->type == tInt2 || token->type == tReal || token->type == tReal2 || token->type == tLiteral);
+}
+
+bool isVar(tToken *token)
+{
+    return token->type == tIdentifier; 
 }
 
 bool isNumber(tSymTable *table, tToken *token)
@@ -159,37 +148,67 @@ int typeToIndex(tTokenType token)
     }
 }
 
-void expression(tStack *stack, tSymTable *table)
+/*
+static int oper_prio(tToken token)
 {
-    if (isOperator(&token) || token.type == tRPar)
+    switch (token.type)
     {
-        dbgMsg("Syntax error: Cannot start expression with %s.\n", tokenName[token.type]);
-        return;
-    }
-    else if (typeToIndex(token) == 14)
-    {
-        dbgMsg("Syntax error: Empty expression.\n");
-        return;
-    }
+    case tMul:
+    case tDiv:
+        return 4;
+        break;
 
-    // We need these pointers to know what exactly should be reduced on the stack.
+    case tPlus:
+    case tMinus:
+        return 3;
+        break;
+    
+    case tLess:
+    case tLessEq:
+    case tMore:
+    case tMoreEq:
+        return 2;
+        break;
+
+    case tIdentical:
+    case tNotIdentical:
+        return 1;
+        break;
+
+    default:
+        break;
+    }
+}
+*/
+
+void expression(tStack *expStack, tSymTable *table)
+{
+    tStack *evalStack;
+    tstack_init(evalStack);
+
+    // We need these pointers to know what exactly should be reduced on the evaluation stack.
     // All the the operators are binary - we dont need more pointers.
     tStackItem *stackTop;
     tStackItem *second;
     tStackItem *third;
+    // Pseudo nonterminals
+    tStackItem intExpression;
+    tStackItem floatExpression;
+    tStackItem stringExpression;
 
-    // Precedence of stack's top token and input token.
+    // Precedence of evaluation stack's top token and input token.
     char precedence;
-    tstack_push(stack, token);
 
-    // Parentheses counter. Left par -> +1, right par -> -1.
-    int parCount = 0;
+    tToken inputToken; 
+    tstack_pop(expStack, &inputToken);
+    tstack_push(evalStack, inputToken);
 
     // We need both empty stack and finishing token to end this loop successfully.
-    while (!tstack_isEmpty(stack) || typeToIndex(token.type) != 14) 
+    while (!tstack_isEmpty(evalStack) || typeToIndex(inputToken.type) != 14) 
     {
+        
         // Definition of the three stack pointers 
-        stackTop = tstack_peek(stack);
+        stackTop = tstack_peek(evalStack);
         if (stackTop != NULL)
         {
             if (stackTop->next != NULL)
@@ -199,25 +218,73 @@ void expression(tStack *stack, tSymTable *table)
                     third = second->next;
             }
         }
+        /*
+        Semantika a pseudokod voleni precedence
 
-        precedence = prd_table[typeToIndex(token.type)][typeToIndex(stack->top->token.type)];
+        if stack top je id
+            if stack second je lpar
+                precedence = shift
+            else
+                if stack second < input
+                    precedence = shift
+                else if stack second > input
+                    precedence = redukce
+                else
+                    precedence = '='
+        else
+            precedence = shift
+        */
+        
+        // We have var/const on top of stack
+        if (isVar(stackTop) || isConst(stackTop))
+            // shifting when stack second is left par
+
+            /*
+            if(isOperator(&inputToken))
+            {
+                
+                // shifting when operator on stack has lower priority than input operator
+                if (oper_prio(second->token) < oper_prio(inputToken))
+                {
+                    precedence = '<';
+                }
+                // reducing when operator on stack has highier priority than input operator
+                else if (oper_prio(second->token) >= oper_prio(inputToken))
+                {
+                    precedence = '>';
+                }
+
+                if (second->token.type == tLPar)
+                {
+                    precedence = '<';
+                }
+            }
+            else if(inputToken.type == tRPar)
+            {
+                if (second->token.type == tLPar)
+                {
+                    precedence = '=';
+                }
+                
+                precedence = '>';
+            }
+            */
+            // Semantika je stejna jako kod nahore, klidne mrkni do tabulky a over si to
+            precedence = prd_table[typeToIndex(second->token.type)][typeToIndex(inputToken.type)];
+        else
+            // Toto je spravne, pokud top je lpar, operator nebo rpar, musime za kazdou cenu dale pushnout
+            precedence = '<';
+
 
         switch (precedence) 
         {
+            case '=':
+                break;
+            
             case '<':
                 // evaluating expression
-
-                // The following token after var/const cannot be var/const or left par
-                // and the following token after operator cannot be operator or right par
-                if ((isVariableOrConst(stackTop) && !isOperator(&token) && token.type != tRPar) || \
-                    (isOperator(stackTop)) && !isVariableOrConst(&token) && token.type != tLPar)
-                {
-                    dbgMsg("Syntax error: Var/const or left par after var/const || Operator or right par after operator.\n");
-                    return;
-                }
-
                 // The following token after literal const or string var has to be any string operator
-                if ((isString(NULL, stackTop) || isString(table, stackTop)) && !stringOp(&token))
+                if ((isString(NULL, stackTop) || isString(table, stackTop)) && !stringOp(&inputToken))
                 {
                     dbgMsg("Semantic error: Missing string operator after string var/const.\n");
                     return;
@@ -226,55 +293,171 @@ void expression(tStack *stack, tSymTable *table)
                 // Stack: string const/var, string operator are on stack but there is no string const/var as input token
                 else if (second != NULL && \
                         (isString(NULL, &second->token) || isString(table, &second->token)) && \
-                        (stringOp(stackTop) && !(isString(NULL, &token) || isString(table, &token))))
+                        (stringOp(stackTop) && !(isString(NULL, &inputToken) || isString(table, &inputToken))))
                 {
                     dbgMsg("Semantic error: String var/const, string operator, missing string var/const.\n");
                     return;
                 }
 
                 // The following token after number var/const has to be any num operator
-                else if ((isNumber(NULL, stackTop) || isNumber(table, stackTop)) && !numberOp(&token))
+                else if ((isNumber(NULL, stackTop) || isNumber(table, stackTop)) && !numberOp(&token) && !relationalOp(&token))
                 {
-                    dbgMsg("Semnatic error: Missing number operator after number var/const.\n");
+                    dbgMsg("Semantic error: Missing number operator after number var/const.\n");
                     return;
                 }
 
                 // Stack: number, number operator are on stack next token has to be number.
-                else if (second != NULL && \
-                        (isNumber(NULL, &second->token) || isNumber(table, &second->token)) && \
-                        (numberOp(stackTop) && !(isNumber(NULL, &token) || isNumber(table, &token))))
+                else if (second != NULL && (isNumber(NULL, &second->token) || isNumber(table, &second->token)) && \
+                        ((numberOp(stackTop) || relationalOp(stackTop)) && !(isNumber(NULL, &inputToken) || isNumber(table, &inputToken))))
                 {
                     dbgMsg("Semantic error: Number var/const, number operator, missing number.\n");
                     return;
                 }
 
-                if (token.type == tLPar)
-                    parCount++;
-                else if (token.type == tRPar)
-                {
-                    parCount--;
-                    if (parCount == -1)
-                    {
-                        dbgMsg("Syntax error: Too much right parentheses.\n");
-                        return;
-                    }
-                }
+                tstack_pop(expStack, &inputToken);
+                tstack_push(evalStack, inputToken);
 
-                tstack_push(stack, token);
-                /*
-                    Prirazeni neni (nevime volani funkci)
-                    Vyhledani prommene v symtable - bude potreba prommena typu pointer na polozku v symtab
-                */
+                if (tstack_isFull(evalStack))
+                    exitError("Expression stack overflow.\n", CERR_INTERNAL);
+
                 break;  
 
             case '>':
                 // evaluating expression
+                /*
+                    jak to bude fungovat:
+
+                    Kdyz se dostaneme sem do redukce, tak nas uz nezajima, co to je za operator a jake ma operandy,
+                    protoze syntakticke chyby se sem nedostanou a semanticke uz mame (snad) vyresene u toho pushovani.
+                    Potrebujem teda de facto vzit ty dva operandy a operator, provest semantickou akci, pripadne nejake
+                    typove konverze, popnout dva operandy a operator a vysledek pushnout zpatky na zasobnik (evalStack). 
+                    Pokud jsou obe promenne, tak provedu operaci a vysledek ulozim do tabulky symbolu (prvniho operandu),
+                    pripadne tedy provedu typovou konverzi tak, ze prepisu typ promenne, kde se ulozil vysledek.
+                    Pokud 1 var a 1 const, pak provedu operaci, vysledek ulozim do symtab, kde je var.
+                    Pokud 2 const, vysledkem je novy token, ktery je taky const.
+                    Trosku na picu, co nam hazi vidle pod nohy je to, ze vsechny data jsou ulozeny v poli znaku a musime si
+                    z toho pripadne vytahnout cisla.
+                    Otazkou je, jestli se s TS bude vubec pracovat a jestli nemame rovnou generovat instrukce.
+                    Sorry chlapi, moc jsem toho nestih, vecer po zapase na to jeste kouknu.
+                */
+                if (tstack_isEmpty(evalStack))
+                {
+                    dbgMsg("Semantic error: Empty stack in expression while trying to reduce.\n");
+                    return;
+                }
+
+                // Typove konverze muzeme udelat rovnou, pokud nejsou definovane, tak to hodi rovnou error.
+                // Rikal jsem si, ze by se rovnou mohly generovat instrukce pro typovou konverzi?
+
+                // Obe ciselne konstanty
+                if (isNumber(NULL, &stackTop->token) && isNumber(NULL, &third->token))
+                {
+
+                }
+                // Prvni promenna, druha konstanta
+                else if (isNumber(table, &stackTop->token) && isNumber(NULL, &third->token))
+                {
+                    if (!isDefined(table, &third->token))
+                    {
+                        dbgMsg("Uninitialized number variable.\n");
+                        return;
+                    }
+                }
+                // Prvni konstanta, druha promenna
+                else if (isNumber(NULL, &stackTop->token) && isNumber(table, &third->token))
+                {
+                    if (!isDefined(table, &third->token))
+                    {
+                        dbgMsg("Uninitialized number variable.\n");
+                        return;
+                    }
+                }
+                // Dve promenne
+                else if (isNumber(table, &stackTop->token) && isNumber(table, &third->token))
+                {
+                    if (!isDefined(table, &third->token) || !isDefined(table, &stackTop->token))
+                    {
+                        dbgMsg("Uninitialized number variable.\n");
+                        return;
+                    }
+                }
+                // Oba retezcove literaly
+                else if (isString(NULL, &stackTop->token) && isString(NULL, &third->token))
+                {
+                    
+                }
+                // Jedna retezcova promenna, jeden literal
+                else if (isString(table, &stackTop->token) && isString(NULL, &third->token))
+                {
+                    if (!isDefined(table, &stackTop->token))
+                    {
+                        dbgMsg("Uninitialized string variable.\n");
+                        return;
+                    }
+                }
+                // Jeden literal, jedna promenna
+                else if (isString(NULL, &stackTop->token) && isString(table, &third->token))
+                {
+                    if (!isDefined(table, &third->token))
+                    {
+                        dbgMsg("Uninitialized string variable.\n");
+                        return;
+                    }
+                }
+                // Zbyvaji dve retezcove promenne 
+                else
+                {
+                    if (!isDefined(table, &third->token) || !isDefined(table, &stackTop->token))
+                    {
+                        dbgMsg("Uninitialized string variable.\n");
+                        return;
+                    }
+                }
+
+
+                dbgMsg("Redukujeme: dva operandy, first: %s, third: %s, operator (second): %s.\n", stackTop->token.data, third->token.data, tokenName[second->token.type]);
+                switch (second->token.type)
+                {
+                    case tPlus:
+
+                        break;
+                    case tMinus:
+
+                        break;
+                    case tMul:
+
+                        break;
+                    case tDiv:
+
+                        break;
+                    case tConcat:
+
+                        break;
+                    case tMore:
+
+                        break;
+                    case tLess:
+
+                        break;
+                    case tMoreEq:
+
+                        break;
+                    case tLessEq:
+
+                        break;
+                    case tIdentical:
+
+                        break;
+                    case tNotIdentical:
+
+                        break;
+                    default:
+                        dbgMsg("Expression: Tady jsme se nikdy nemeli dostat, kurnik.\n");
+                        break;
+                }
+
                 // code generation
                 break;
-
-            default:
-                dbgMsg("Syntax error: Expected different token.\n");
-                return;
         }
         nextToken();
     } 
