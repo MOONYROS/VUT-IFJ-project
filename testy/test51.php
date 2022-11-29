e62e3de2b032ae3b33b7aaed81e5f576f3f7cd77
MeRohel <milanekmenc@seznam.cz>
2022-11-29 18:08:46 +0100

    tests repaired hopefuly

--- testy/test51.php
+++ testy/test51.php
@@ -5,5 +5,5 @@ $num = 4;
 $num2 = 3;
 $num3 = 6.0;
 $num4 = 3.0;
-$id1 = mul($num, $num3);
+$id1 = mul(int $num,int $num3);
 ?>
--- testy/test52.php
+++ testy/test52.php
@@ -5,5 +5,5 @@ $num = 4;
 $num2 = 3;
 $num3 = 6.0;
 $num4 = 3.0;
-$id1 = div($num, $num3);
+$id1 = div(int $num,int $num3);
 ?>
