dfbef3960df61db2f91f4b7503360b9282c997cc
MeRohel <milanekmenc@seznam.cz>
2022-11-29 17:28:15 +0100

    Dalsi testy

---
+++ testy/test48.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+// Melo by FUNGOVAT po deklaraci fcí
+$num = 4;
+$num2 = 3;
+$num3 = 6.0;
+$num4 = 3.0;
+$id1 = add($num, $num2);
+$id6 = add($num3, $num4);
+$id2 = sub($num, $num2);
+$id7 = sub($num3, $num4);
+$id3 = mul($num, $num2);
+$id8 = mul($num3, $num4);
+$id4 = div($num3, $num4);
+$id5 = idiv($num, $num2);
+?>
---
+++ testy/test52.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+// NEFUNGUJE
+$num = 4;
+$num2 = 3;
+$num3 = 6.0;
+$num4 = 3.0;
+$id1 = div($num, $num3);
+?>
